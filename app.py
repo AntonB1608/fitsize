89d24cfd53536f99e205aa134264aa00f6306143
Anton Boewer <antonboewer@icloud.com>
2026-08-07 15:39:05 +0200

    wip: start resize fallback for unreachable target sizes

--- app.py
+++ app.py
@@ -5,19 +5,24 @@ from io import BytesIO
 ziel = 5000 * 1024
 niedrig_quali = 30
 hohe_quali = 95
+local = True
+if local:
+    img = Image.open("original.jpg")
+else:
+    img = request.form("img")
 
 
 def compress_to_target(ziel, niedrig_quali, hohe_quali):
     
-    img = Image.open("test.jpg")   
+    img = Image.open("original.jpg")   
     
     if img.mode in ("RGBA", "P", "LA"):   
         img = img.convert("RGB")  
     
-
     beste_quali = None
     beste_groesse = None
     bester_buffer = None
+    kleiner = None
     while niedrig_quali <= hohe_quali:
         mitte = (niedrig_quali + hohe_quali) // 2
 
@@ -34,14 +39,30 @@ def compress_to_target(ziel, niedrig_quali, hohe_quali):
             
         else:
             hohe_quali = mitte - 1
-            
+           
     
-    if beste_quali is None:
-        erfolg = False
-        return erfolg, None, None, None
-    else:
+    if not beste_quali is None:
         erfolg = True
-        return erfolg, beste_quali, beste_groesse, bester_buffer
+        return erfolg, beste_quali, beste_groesse, bester_buffer, None
+    
+
+    while not size >= 600:
+        size = height * width
+        if size > 600:
+            erfolg = True
+            kleiner = img.resize((height, width))
+            height * 
+            return erfolg, None, None, None
+        else:
+            erfolg = False
+            return erfolg, None, None, None, kleiner
+
+
+
+
+
+        
+        
 
 
 erfolg, beste_quali, beste_groesse, bester_buffer = compress_to_target(ziel, niedrig_quali, hohe_quali)
