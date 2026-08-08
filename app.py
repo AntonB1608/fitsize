481bc99e4a3abd87f3af20a2119329114e43df9b
Anton Boewer <antonboewer@icloud.com>
2026-08-08 20:36:00 +0200

    refactor: extract quality search into its own function

--- app.py
+++ app.py
@@ -2,21 +2,15 @@ from PIL import Image
 from io import BytesIO
 
 
-ziel = 5000 * 1024
-niedrig_quali = 30
-hohe_quali = 95
-local = True
-if local:
-    img = Image.open("original.jpg")
-else:
-    img = request.form("img")
-
-
-def compress_to_target(ziel, niedrig_quali, hohe_quali):
+ziel = 50 * 1024
+img = Image.open("original.jpg")   
+def find_quali(img, ziel):
+    niedrig_quali = 30
+    hohe_quali = 95
+   
     
-    img = Image.open("original.jpg")   
-    
-    if img.mode in ("RGBA", "P", "LA"):   
+    if img.mode in ("RGBA", "P", "LA"):  
+
         img = img.convert("RGB")  
     
     beste_quali = None
@@ -24,6 +18,7 @@ def compress_to_target(ziel, niedrig_quali, hohe_quali):
     bester_buffer = None
     kleiner = None
     while niedrig_quali <= hohe_quali:
+
         mitte = (niedrig_quali + hohe_quali) // 2
 
         buffer = BytesIO()
@@ -31,6 +26,7 @@ def compress_to_target(ziel, niedrig_quali, hohe_quali):
         groesse = buffer.tell()
 
         if groesse <= ziel:
+
             beste_quali = mitte
             beste_groesse = groesse
             bester_buffer = buffer
@@ -42,37 +38,20 @@ def compress_to_target(ziel, niedrig_quali, hohe_quali):
            
     
     if not beste_quali is None:
-        erfolg = True
-        return erfolg, beste_quali, beste_groesse, bester_buffer, None
-    
 
-    while not size >= 600:
-        size = height * width
-        if size > 600:
-            erfolg = True
-            kleiner = img.resize((height, width))
-            height * 
-            return erfolg, None, None, None
-        else:
-            erfolg = False
-            return erfolg, None, None, None, kleiner
+        erfolg_quali = True
+        return erfolg_quali, beste_quali, beste_groesse, bester_buffer
+    else:
+        erfolg_quali = False
+        return erfolg_quali, beste_quali, beste_groesse, bester_buffer
 
 
 
 
 
-        
-        
-
-
-erfolg, beste_quali, beste_groesse, bester_buffer = compress_to_target(ziel, niedrig_quali, hohe_quali)
-
-if erfolg:
-    print(f"Qualität {quali}, {groesse} Bytes")
-    with open("ergebnis.jpg", "wb") as f:
-        f.write(buffer.getvalue())
-else:
-    print("Zielgröße nicht erreichbar")
+erfolg_quali, beste_quali, beste_groesse, bester_buffer = find_quali(img, ziel)
+if erfolg_quali:
+    print(f"Qualität {beste_quali}, {beste_groesse} Bytes")
     
     
     
